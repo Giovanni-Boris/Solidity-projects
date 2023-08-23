dfc02b5303d805be500c405b3c4018e541ce472c
Boris Giovanni <bancasi@unsa.edu.pe>
2023-08-23 11:21:34 -0500

    donate to camping creadted

--- crowdfunding/web3/contracts/Contract.sol
+++ crowdfunding/web3/contracts/Contract.sol
@@ -35,6 +35,18 @@ contract MyContract {
 
         return numberOfCampaigns++;
     }
+    function donateToCampaign(uint256 _id) public payable{
+        uint256 amount = msg.value;
+        Campaign storage campaign  = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
 
 
