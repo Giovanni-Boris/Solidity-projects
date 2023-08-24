0c8dad2f74b13e8f71164446ae08b41db35bd128
Boris Giovanni <bancasi@unsa.edu.pe>
2023-08-24 16:32:13 -0500

    FormField component created

--- crowdfunding/front/src/components/index.js
+++ crowdfunding/front/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar.jsx";
 export { default as Sidebar } from "./Sidebar.jsx";
 export { default as CustomButton } from "./CustomButton";
+export { default as FormField } from "./FormField";
