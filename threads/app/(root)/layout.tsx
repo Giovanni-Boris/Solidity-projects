ff2c848f4344fd85d18f22179a33bd6013bc13cc
Boris Giovanni <bancasi@unsa.edu.pe>
2023-08-30 10:56:07 -0500

    createtion of components with teh layou of pages

--- threads/app/(root)/layout.tsx
+++ threads/app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 export const metadata = {
   title: "Threads",
@@ -16,7 +20,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} `}>{children}</body>
+        <body className={`${inter.className} `}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
