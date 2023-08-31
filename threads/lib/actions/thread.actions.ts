f7a3b5a759d64572064459cabb0afeecc30ba16d
Boris Giovanni <bancasi@unsa.edu.pe>
2023-08-31 12:05:36 -0500

    createTHread created"

---
+++ threads/lib/actions/thread.actions.ts
@@ -0,0 +1,41 @@
+"use server";
+import Thread from "../models/Thread.model";
+import { connectToDB } from "../mongoose";
+import User from "../models/user.model";
+
+interface Params {
+  text: string;
+  author: string;
+  comunityId: string || null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  comunityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
+
+    const createThread =await Thread.create(
+      {
+        text,
+        author,
+        community: null,
+      },
+      { upsert: true },
+    );
+    //Update user model
+    await User.findByIdAndUpdate(author, {
+      $push: {threads: createThread._id}
+    })
+    revalidatePath(path);
+
+    
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
