40c302f95943751af271668503762da97649f8dc
Boris Giovanni <bancasi@unsa.edu.pe>
2023-09-01 11:42:51 -0500

    Usercard component created+

--- threads/app/(root)/search/page.tsx
+++ threads/app/(root)/search/page.tsx
@@ -1,9 +1,57 @@
-const page = () => {
+import Image from "next/image";
+
+import { fetchUsers, fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { profileTabs } from "@/constants";
+import UserCard from "@/components/cards/UserCard";
+
+import PostThread from "@/components/forms/PostThread";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  /*const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");*/
+
+  // fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
       <h1 className="head-text">Search</h1>
+
+      {/* Seach Bar*/}
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
+}
 
-export default page;
+export default Page;
