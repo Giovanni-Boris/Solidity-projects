a8e084e68fae8db70dce129f3fa3739e2401cb00
Boris Giovanni <bancasi@unsa.edu.pe>
2023-09-02 10:59:33 -0500

    changin webhook key

--- threads/app/api/webhook/clerk/route.ts
+++ threads/app/api/webhook/clerk/route.ts
@@ -46,7 +46,7 @@ export const POST = async (request: Request) => {
 
   // Activitate Webhook in the Clerk Dashboard.
   // After adding the endpoint, you'll see the secret on the right side.
-  const wh = new Webhook("whsec_EF6TORt5QcKF4JPHgiwEdRcjZZjaY1Hh");
+  const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
   let evnt: Event | null = null;
 
   try {
